6702b115e44bc758f51da57ae010684aa239c0b6
TheusXD <matheus_silva4412@hotmail.com>
2026-04-28 19:43:06 -0300

    fix: downgrade jspdf to 2.5.1 as 2.5.2 is not available on cdnjs

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'checklist-campo-v11';
+const CACHE_NAME = 'checklist-campo-v12';
 const STATIC_ASSETS = [
   './',
   './index.html',
@@ -23,7 +23,7 @@ const STATIC_ASSETS = [
 
 const CDN_ASSETS = [
   'https://unpkg.com/dexie@4.0.11/dist/dexie.min.js',
-  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.2/jspdf.umd.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
   'https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.8.4/jspdf.plugin.autotable.min.js'
 ];
 
